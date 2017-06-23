4b428a57275618e959f726bd20f1ff3afcc79b32
sanjsanj <sanjsanj@hotmail.com>
2017-06-23 20:48:28 +0100

    Fix e2e tests

--- test/specs/e2e-tests.js
+++ test/specs/e2e-tests.js
@@ -12,6 +12,7 @@ describe('TodoList App', () => {
 
   it('Should load with the right title', () => {
     const actualTitle = browser.getTitle();
+
     expect(actualTitle).to.eql('ToDo List');
   });
 
@@ -20,21 +21,53 @@ describe('TodoList App', () => {
     browser.click('.todo-submit');
     actual = browser.element('.todo-text').getText();
     expected = 'Get better at testing';
+
     expect(actual).to.equal(expected);
   });
 
-  it('Should allow me to toggle my Todo to done', () => {
+  it('Should set new todos to incomplete', () => {
     browser.element('.todo-input').setValue('Get better at testing');
     browser.click('.todo-submit');
-    actual = browser.element('.todo.done');
+    actual = browser.element('.completed');
+
     expect(actual.state).to.equal('failure');
+  });
+
+  it('Should allow me to toggle my Todo to completed', () => {
+    browser.element('.todo-input').setValue('Get better at testing');
+    browser.click('.todo-submit');
     browser.click('.todo-text');
+    actual = browser.element('.completed');
+
     expect(actual.state).to.equal('success');
   });
 
+  it('Should allow me to toggle my Todo to completed and back to incomplete', () => {
+    browser.element('.todo-input').setValue('Get better at testing');
+    browser.click('.todo-submit');
+    browser.click('.todo-text');
+    browser.click('.todo-text');
+    actual = browser.element('.completed');
+
+    expect(actual.state).to.equal('failure');
+  });
+
   it('Should allow me to delete my Todo', () => {
+    browser.element('.todo-input').setValue('Get better at testing');
+    browser.click('.todo-submit');
+    browser.click('.delete-todo');
+    actual = browser.element('.todo');
+
+    expect(actual.state).to.equal('failure');
   });
 
   it('Should allow me to undelete my Todo', () => {
+    browser.element('.todo-input').setValue('Get better at testing');
+    browser.click('.todo-submit');
+    browser.click('.delete-todo');
+    browser.click('.undelete-todo');
+    actual = browser.element('.todo');
+
+    expect(actual.state).to.equal('success');
   });
 });
